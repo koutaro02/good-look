81ed8990a87ab7b7ab5333ce8582252f5cfb6731
koutaro02 <k.u89yakyuu@icloud.com>
2021-02-01 18:03:06 +0900

    userのモデル作成
    それに紐付くモデルも作成

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :family_name
+    validates :last_name
+    validates :phone_number
+    validates :company_name
+    validates :position
+  end
+end
